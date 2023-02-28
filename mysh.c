f5f19b55f8e313a22b93d687721f6ee46033df0d
JasonGuo00 <jasonglobguo@gmail.com>
2023-02-28 18:07:44 -0500

    File reading
    Scuffed implementation of file reading for batch and implementation mode

--- mysh.c
+++ mysh.c
@@ -3,12 +3,29 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define BUF_SIZE 4096
+
+
+char* lineBuffer;
+int lineLength;
 
 int cd(char* path) {
-    return chdir(path);
+    // Check if path is accessible
+    struct stat block;
+    if(stat(path, &block) == -1) {
+        return -1;
+    }
+    // Change directory
+    else {
+      return chdir(path);  
+    }
 }
 
 void pwd() {
+    // Change the size of the buffer until it can encapsulate the path
     int size = 100, complete = 0;
     char* buffer = malloc(sizeof(char)*100);
     while (!complete) {
@@ -20,23 +37,83 @@ void pwd() {
             buffer = (char*)realloc(buffer, size);
         }
     }
-    printf("%s\n", buffer);
+    // Find the size of the path
+    int pathSize = 0;
+    for(int i = 0; i < size; i++) {
+        pathSize++;
+        if(buffer[i] == '\0') {
+            break;
+        }
+    }
+    // Some polishing
+    buffer[pathSize] = '\n';
+    buffer[pathSize+1] = '\0';
+    // Return path, free buffer
+    write(1, buffer, pathSize+1);
+    free(buffer);
 }
 
+
 int main(int argc, char* argv[]) {
+    int file, bytes;
+    char* buffer = malloc(sizeof(char)*BUF_SIZE);
+    lineBuffer = malloc(sizeof(char)*BUF_SIZE);
+
+    // Check whether we're in batch mode, interactive mode, or too many arguments were passed
     if(argc == 1) {
         //interactive mode
         printf("Welcome to my shell!\n");
-        int exit = 0;
-        while(!exit) {
-            printf("mysh> ");
+        // input loop
+        file = 0;
+        write(1, "mysh> ", 7);
+        while((bytes = read(file, buffer, BUF_SIZE)) > 0) {
+            memcpy(lineBuffer, buffer, bytes);
+            lineBuffer[bytes] = '\0';
+            // Do something with lineBuffer here
+
+            // Exit command for testing
+            if(strcmp(lineBuffer, "exit\n") == 0) {
+                close(file);
+                free(buffer);
+                free(lineBuffer);
+                return EXIT_SUCCESS;
+            }
+            else if(strcmp(lineBuffer, "pwd\n") == 0) {
+                pwd();
+            }
+            write(1, "mysh> ", 7);
         }
     }
     else if(argc == 2) {
         //batch mode
+        file = open(argv[1], O_RDONLY);
+        int i;
+        int lineStart = 0;
+        while((bytes = read(file, buffer, BUF_SIZE)) > 0) {
+            for(i = 0; i < bytes; i++) {
+                if(buffer[i] == '\n') {
+                    lineLength = i - lineStart + 1;
+                    memcpy(lineBuffer, buffer + lineStart, bytes);
+                    lineBuffer[lineLength] = '\0';
+                    write(1, lineBuffer, lineLength);
+                    // Do something with lineBuffer here
+                    // Exit command for testing
+                    if(strcmp(lineBuffer, "exit\n") == 0) {
+                        close(file);
+                        free(buffer);
+                        free(lineBuffer);
+                        return EXIT_SUCCESS;
+                    }
+                    else if(strcmp(lineBuffer, "pwd\n") == 0) {
+                        pwd();
+                    }
+                    lineStart = i+1;
+                }
+            }
+        }
+        // If the last character in the file wasn't a '/n', then we have a partial command: just ignore it.
     }
     else {
         printf("Error: Too many arguments.");
     }
-    printf("%d\n", argc);
 }
